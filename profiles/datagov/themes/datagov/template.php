e45530867d41e54419c6bd5bd289433f7ecee7a8
Dan Olaru <dan.olaru@reisystems.com>
2013-01-22 10:40:00 -0500

    removed webtrends code, removed 'data.gov' label from search block

--- profiles/datagov/themes/datagov/template.php
+++ profiles/datagov/themes/datagov/template.php
@@ -28,7 +28,7 @@ function datagov_form_alter(&$form, &$form_state, $form_id) {
   $form['search_block_form']['#title'] = t('Search'); // Change the text on the label element
   $form['search_block_form']['#title_display'] = 'invisible'; // Toggle label visibilty
   $form['search_block_form']['#size'] = 40;  // define size of the textfield
-  $form['search_block_form']['#default_value'] = t('Search ') . $placeholder; // Set a default value for the textfield
+  $form['search_block_form']['#default_value'] = t('Search '); // Set a default value for the textfield
 
   // Add extra attributes to the text box
   $form['search_block_form']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search " . $placeholder . ";}";
